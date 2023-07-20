811ec2258943d27082b483301206b6f24509834a
ArturT <mr.arturio@gmail.com>
2023-07-20 18:48:09 -0400

    mode dynamic pages + upload data.json

---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,13 @@
+export default function Page (){
+  return  (
+    <div>
+  <h1>Events page</h1>
+  <a href='/event/event1'> Event 1</a>
+  <a href='/event/event2'> Event 2</a>
+  <a href='/event/event3'> Event 3</a>
+  <a href='/event/event4'> Event 4</a>
+  <a href='/event/event5'> Event 5</a>
+  <a href='/event/event6'> Event 6</a>
+  </div>
+  );
+};
