ecc57f0f8c8ab22a671af3b6016335b5071354c5
ArturT <mr.arturio@gmail.com>
2023-07-22 23:01:03 -0400

    use _app, create main-layout, move footer/header there

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,17 @@ import React from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { HomePage } from "../src/components/home/home-page";
-import { Footer } from "../src/components/footer/footer";
-import { Header } from "../src/components/header/header";
 
 export default function Home({ data }) {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Events App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-
       <HomePage data={data} />
-
-      <Footer />
+      
     </div>
   );
 }
