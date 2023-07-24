255350e65f4f803a78ea09ceeaf2a3ea914d3200
ArturT <mr.arturio@gmail.com>
2023-07-23 22:53:41 -0400

    more style + edit home-pagr file

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 
 export const HomePage = ({ data }) => (
-  <main>
+  <div className="home_body">
     {data.map((ev) => (
       <Link key={ev.id} href={`/events/${ev.id}`} passHref>
-        <Image width={200} height={200} alt={ev.title} src={ev.image} />
+        <div className="image">
+          <Image width={200} height={200} alt={ev.title} src={ev.image} />
+        </div>
         <h2> {ev.title} </h2>
         <p> {ev.description} </p>
       </Link>
     ))}
-  </main>
+  </div>
 );
