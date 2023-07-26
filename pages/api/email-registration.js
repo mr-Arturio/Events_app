f6e49a455c0b95427e133cdccf8a425edf5a67fc
ArturT <mr.arturio@gmail.com>
2023-07-25 23:33:23 -0400

    add comments and notes to code

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,22 +1,31 @@
+//used to handle file paths.
 import path from 'path';
+//used to read and write files.
 import fs from 'fs';
 
 function buildPath() {
+  //combines the current working directory with 'data' and 'data.json'
   return path.join(process.cwd(), 'data', 'data.json');
 }
 
+//takes the file path as an argument and reads the contents of data.json
 function extractData(filePath) {
   const jsonData = fs.readFileSync(filePath);
   const data = JSON.parse(jsonData);
   return data;
 }
 
+// default export of the API file
 export default function handler(req, res) {
   const { method } = req;
 
+  //The file path to data.json is obtained by calling the buildPath function.
   const filePath = buildPath();
+  // data from data.json is extracted by calling the extractData function. 
+  //The events_categories and allEvents properties are destructured from the returned data.
   const { events_categories, allEvents } = extractData(filePath);
 
+  //if fail
   if (!allEvents) {
     return res.status(404).json({
       status: 404,
@@ -24,13 +33,17 @@ export default function handler(req, res) {
     });
   }
 
+  //when form submitted
   if (method === 'POST') {
+    //data extracted from the request body.
     const { email, eventId } = req.body;
 
+    //if empty or no @
     if (!email | !email.includes('@')) {
       res.status(422).json({ message: 'Invalid email address' });
     }
 
+    //mapped over to find the event with the matching eventId
     const newAllEvents = allEvents.map((ev) => {
       if (ev.id === eventId) {
         if (ev.emails_registered.includes(email)) {
@@ -45,6 +58,7 @@ export default function handler(req, res) {
       return ev;
     });
 
+    //The allEvents array is updated to include the newly registered email, and the data is written back to the data.json file.
     fs.writeFileSync(filePath, JSON.stringify({ events_categories, allEvents: newAllEvents }));
 
     res.status(201).json({
--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -3,33 +3,49 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 
 const SingleEvent = ({ data }) => {
-  const inputEmail = useRef();
+  // A reference to the input field with the ID email 
+  //useRef hook is used to access the input's value later.
+  const inputEmail = useRef(); 
+  //useRouter hook is used to access the Next.js router object.
+  //allows the component to access the query parameters, such as id in the URL.
   const router = useRouter();
+  //hook will be used to store and display messages to the user (e.g., validation errors or registration success messages).
   const [message, setMessage] = useState('');
 
+  // function, which is called when the user submits the form
   const onSubmit = async (e) => {
+    //prevents page refresh.
     e.preventDefault();
+    //rerrive email input value from the 'ref'
     const emailValue = inputEmail.current.value;
+    //The event ID is extracted from the router's query parameters.
+    //The ? is used for optional chaining to avoid errors if the router.query object is not defined.
     const eventId = router?.query.id;
 
+    // used to validate the email address format.
     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if (!emailValue.match(validRegex)) {
-      setMessage('Please introduce a correct email address');
+      setMessage('Kindly provide a valid email address.');
     }
 
+    //handles the email registration process
     try {
       const response = await fetch('/api/email-registration', {
         method: 'POST',
-        headers: {
+        headers: { //specify that the content being sent is in JSON format
           'Content-Type': 'application/json',
         },
+        //The data to be sent in the request body is an object containing email and eventId
+        //which are extracted from the form input and the router, respectively.
         body: JSON.stringify({ email: emailValue, eventId }),
       });
 
+      //if fails
       if (!response.ok) throw new Error(`Error: ${response.status}`);
       const data = await response.json();
       setMessage(data.message);
+      //The email input field is cleared after the successful registration or when an error occurs.
       inputEmail.current.value = '';
     } catch (e) {
       console.log('ERROR', e);
@@ -49,7 +65,7 @@ const SingleEvent = ({ data }) => {
           id="email"
           placeholder="Type your email here..."
         />
-        <button type="butoon">Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
