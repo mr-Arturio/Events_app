02bda3f870094ab7872191b97096d1f368642b9a
ArturT <mr.arturio@gmail.com>
2023-07-25 23:53:51 -0400

    minor fix

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -28,7 +28,6 @@ export default function handler(req, res) {
   //if fail
   if (!allEvents) {
     return res.status(404).json({
-      status: 404,
       message: 'Events data not found',
     });
   }
